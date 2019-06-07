d56802192d4fd7dd237a392803b0c7e502a4fa00
Vítor Borba Rodrigues <vitorborbarodrigues@gmail.com>
2019-06-07 12:03:54 -0300

    Add script

---
+++ style_transfer.py
@@ -0,0 +1,160 @@
+# import libs
+from PIL import Image
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.optim as optim
+import requests
+from torchvision import transforms, models
+
+# get features from pre-trained model (VGG19) - No need for the dense layers
+vgg = models.vgg19(pretrained=True).features
+for param in vgg.parameters():
+    param.requires_grad_(False)
+
+def load_image(img_path, max_size=600, shape=None):
+    ''' Load in and transform an image, making sure the image
+       is <= 400 pixels in the x-y dims.'''
+
+    image = Image.open(img_path).convert('RGB')
+    
+    # large images will slow down processing
+    if max(image.size) > max_size:
+        size = max_size
+    else:
+        size = max(image.size)
+    
+    if shape is not None:
+        size = shape
+        
+    # Make sure to normalize the images according to the VGG19 requirements
+    in_transform = transforms.Compose([
+                        transforms.Resize(size),
+                        transforms.ToTensor(),
+                        transforms.Normalize((0.485, 0.456, 0.406), 
+                                             (0.229, 0.224, 0.225))])
+
+    # discard the transparent, alpha channel (that's the :3) and add the batch dimension
+    image = in_transform(image)[:3,:,:].unsqueeze(0)
+    
+    return image
+
+# helper function for un-normalizing an image 
+# and converting it from a Tensor image to a NumPy image for display
+def im_convert(tensor):
+    """ Display a tensor as an image. """
+    
+    image = tensor.to("cpu").clone().detach()
+    image = image.numpy().squeeze()
+    image = image.transpose(1,2,0)
+    image = image * np.array((0.229, 0.224, 0.225)) + np.array((0.485, 0.456, 0.406))
+    image = image.clip(0, 1)
+
+    return image
+
+content = load_image(r'imgs/...') # MAKE SURE TO REPLACE THE THREE DOTS WITH YOUR IMAGE
+style = load_image('images/strips.jpg', shape=content.shape[-2:])
+
+def get_features(image, model, layers=None):
+    """ Run an image forward through a model and get the features for 
+        a set of layers. Default layers are for VGGNet matching Gatys et al (2016)
+    """
+    
+    ## Need the layers for the content and style representations of an image
+    if layers is None:
+        layers = {'0': 'conv1_1', 
+                 "5": "conv2_1", 
+                 "10": "conv3_1", 
+                 "19": "conv4_1", 
+                 '21': 'conv4_2',
+                 "28": "conv5_1"}
+        
+    features = {}
+    x = image
+    # model._modules is a dictionary holding each module in the model
+    for name, layer in model._modules.items():
+        x = layer(x)
+        if name in layers:
+            features[layers[name]] = x
+            
+    return features
+
+def gram_matrix(tensor):
+    """ Calculate the Gram Matrix of a given tensor 
+        Gram Matrix: https://en.wikipedia.org/wiki/Gramian_matrix
+    """
+    
+    _, d, h, w = tensor.size()
+    tensor = tensor.view(d, h*w)
+    gram = torch.mm(tensor, tensor.t())
+    
+    return gram
+
+# get content and style features only once before forming the target image
+content_features = get_features(content, vgg)
+style_features = get_features(style, vgg)
+
+# calculate the gram matrices for each layer of our style representation
+style_grams = {layer: gram_matrix(style_features[layer]) for layer in style_features}
+
+# create a third "target" image and prep it for change
+# it is a good idea to start of with the target as a copy of our *content* image
+# then iteratively change its style
+target = content.clone().requires_grad_(True)
+
+style_weights = {'conv1_1': 1.0,
+                 'conv2_1': 1.0,
+                 'conv3_1': 0.8,
+                 'conv4_1': 0.3,
+                 'conv5_1': 0.1}
+
+content_weight = 1  # alpha
+style_weight = 1e12  # beta
+
+optimizer = optim.Adam([target], lr=0.005)
+steps = 5000
+
+for i in range(1, steps+1):
+
+    target_features = get_features(target, vgg)
+    content_loss = torch.mean((target_features["conv4_2"] - content_features["conv4_2"]) ** 2)
+    
+    # initialize the style loss to 0
+    style_loss = 0
+
+    for layer in style_weights:
+        # get the "target" style representation for the layer
+        target_feature = target_features[layer]
+        _, d, h, w = target_feature.shape
+        
+        target_gram = gram_matrix(target_feature)
+        
+        style_gram = style_grams[layer]
+
+        layer_style_loss = style_weights[layer] * torch.mean((target_gram - style_gram) ** 2)
+        
+        # add to the style loss
+        style_loss += layer_style_loss / (d * h * w)
+        
+        
+    total_loss = content_weight * content_loss + style_weight * style_loss
+    
+    # update target image
+    optimizer.zero_grad()
+    total_loss.backward()
+    optimizer.step()
+    
+    # display intermediate images and print the loss
+    if  i % show_every == 0:
+        print('Total loss: ', total_loss.item())
+        plt.imshow(im_convert(target))
+        plt.show()
+
+# display content and final, target image
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(30, 10))
+ax1.imshow(im_convert(content))
+ax1.axis("off")
+ax2.imshow(im_convert(style))
+ax2.axis("off")
+ax3.imshow(im_convert(target))
+ax3.axis("off")
